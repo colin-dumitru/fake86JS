44d892506e2246a09da4a364b5aca73a2e0eaba4
Catalin Dumitru <colin.dumitru@gmail.com>
2013-06-02 22:47:17 +0300

    finished gh pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,13 @@
 function onLoad() {
     $("#loadButton").click(function () {
+		$("#loadingOverlay").addClass("shown");
+
         $("#mainContent")
             .text("Loading...")
             .removeClass("wrapper")
             .addClass("mainContentExpanded")
-            .load("fake86.html");
+            .load("fake86.html", function() {
+            	$("#loadingOverlay").removeClass("shown");
+            });
     });
 }
